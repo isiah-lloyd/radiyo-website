b777af7b9d177d380946a79cae216be02b338353
Isiah Lloyd <robotdude101@gmail.com>
2022-01-10 21:56:22 -0500

    Add legal mumbo jumbo

---
+++ src/routes/privacy.svelte
@@ -0,0 +1,20 @@
+<h1>Privacy</h1>
+
+<h2>Google Analytics</h2>
+<p>This website uses Google Analytics for well, analytical reasons. If you wish to opt-out of Google Analytics tracking use an adblocker or download the <a href="https://tools.google.com/dlpage/gaoptout">opt-out extension</a>.</p>
+
+<h2>Data we collect and store during usage of RadiYo</h2>
+<p>When you use RadiYo we may collect and store the following information:</p>
+<ul>
+    <li>guild name/id</li>
+    <li>user name/id (only when user uses bot)</li>
+    <li>stations you listen to</li>
+    <li>search queries</li>
+</ul>
+<p>this information is used to enhance the bot and for debugging purposes</p>
+
+<h2>Data deletion</h2>
+<p>If you want to delete your data, please fill out this <a href="https://forms.gle/trTpz1PhaTNWZeLV6">form></a>.</p>
+
+<h2>Information</h2>
+<p>For more support about your data/privacy, email radiyo@isiah.me</p>
